df55cf0bb57ce2844ff10dee3e909585e0364b17
AJ Morris <ajmorris2002@gmail.com>
2014-06-23 23:08:17 -0400

    Hiding this for right now.
    Concat.js is not working.

--- grunt/concat.js
+++ grunt/concat.js
@@ -1,12 +1,12 @@
-module.exports = {
-  all: {
-    files: {
-        'js/min/plugins.js': [
-        ],
-        'js/min/matchMedia.js': [
-        ],
-        'js/min/script.js': [
-        ]
-    }
-  }
-};
+// module.exports = {
+//   all: {
+//     files: {
+//         'js/min/plugins.js': [
+//         ],
+//         'js/min/matchMedia.js': [
+//         ],
+//         'js/min/script.js': [
+//         ]
+//     }
+//   }
+// };
